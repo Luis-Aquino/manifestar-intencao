79a5586259e75e66dccbd5c8c316f0f178620f15
Henrique Rodrigues Araujo <55699671+HenriqueRodriguesA@users.noreply.github.com>
2025-06-04 20:08:12 -0300

    Update NucleoService.java
    Faltou os pacotes de importação

--- src/main/java/br/universidade/manifestarintencao/service/NucleoService.java
+++ src/main/java/br/universidade/manifestarintencao/service/NucleoService.java
@@ -1,3 +1,11 @@
+package br.universidade.manifestarintencao.service;
+
+import br.universidade.manifestarintencao.entity.Nucleo;
+import br.universidade.manifestarintencao.repository.NucleoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class NucleoService {
     private final NucleoRepository nucleoRepository;
